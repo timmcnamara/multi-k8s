67c55baa07af641ebf1df38c061ed396f46a22bb
Tim McNamara <timlmcn@gmail.com>
2019-02-22 15:00:14 +1100

    update yml name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t timmcnamaradev/multi-client:latest -t timmcnamaradev/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t timmcnamaradev/multi-server:latest -t timmcnamaradev/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t timmcnamaradev/multi-worker:latest -t timmcnamaradev/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push timmcnamaradev/multi-client:latest
 docker push timmcnamaradev/multi-server:latest
 docker push timmcnamaradev/multi-worker:latest
